7ee3c624262f69e18a83a68973311e789e3b38de
Laura Fiig <Laura Fiig>
2016-11-08 18:30:17 -0600

    Game completed.  Win loss tracking and reset implemented.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,39 +11,75 @@ var count1 = Math.floor(Math.random() * 12) + 1
 var count2 = Math.floor(Math.random() * 12) + 1
 var count3 = Math.floor(Math.random() * 12) + 1
 var count4 = Math.floor(Math.random() * 12) + 1
-console.log(count1,count2,count3,count4)
-//display initial stats on game board
 
 $(document).ready(function() {
 
+//display initial stats on game board
 $("#goalVal").html(goal);
 $("#scoreVal").html(score);
 $("#numWins").html(wins);
 $("#numLosses").html(losses);
 
-//var score=score+count1
-//console.log(score)
-//for (i=1; 1<5; 1++){
-	
+//reset function
+function reset () {
+//reset all variables except win/loss counters
+//including new random number generation
+score = 0;
+goal = Math.floor(Math.random() * 102) + 19;
+count1 = Math.floor(Math.random() * 12) + 1;
+count2 = Math.floor(Math.random() * 12) + 1;
+count3 = Math.floor(Math.random() * 12) + 1;
+count4 = Math.floor(Math.random() * 12) + 1;
+//reset display of goals and score
+$("#goalVal").html(goal);
+$("#scoreVal").html(score);
+};
+
+//function to increase score, then check
+// for win/loss condition
+function collect() {
+	//goal not reached,  log score and continue play
+	if ((score) < (goal)) {
+		$("#scoreVal").html(score);
+	//passed goal, loss condition 
+	//log score, record and alert loss, reset
+	} else if ((score) > (goal)) {
+		$("#scoreVal").html(score);
+		losses++;
+		$("#numLosses").html(losses);
+		alert("you lose");
+		reset();
+//matched goal, win condition 
+//log score, record and alert win, reset
+	} else {
+		$("#scoreVal").html(score);
+		wins++;	
+		$("#numWins").html(wins);
+		alert("you win");
+		reset();
+}
+
+}
+
+//add value of crystal one
 $("#crystal1").on("click",function(){
 	score += count1;
-	$("#scoreVal").html(score);
+	collect()
 });
-	
+//add value of crystal two	
 $("#crystal2").on("click",function(){
 	score += count2;
-	$("#scoreVal").html(score);
+	collect()	
 });
-	
+//add value of crystal three	
 $("#crystal3").on("click",function(){
 	score += count3;
-	$("#scoreVal").html(score);
+	collect()	
 });
-	
+//add value of crystal four	
 $("#crystal4").on("click",function(){
 	score += count4;
-	$("#scoreVal").html(score);
+		collect()
 });
-
 
 });
